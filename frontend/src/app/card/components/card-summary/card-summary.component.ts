7af4294d3a20f2a234a9054ef3e5850cf2a0227c
Miguel Alanis <anamelessdeath@gmail.com>
2024-06-15 20:16:19 -0600

    card index layout and card list components

---
+++ frontend/src/app/card/components/card-summary/card-summary.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { CardItem, EMPTY_CARD_ITEM } from '@common/types/cardItem';
+
+@Component({
+  selector: 'app-card-summary',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './card-summary.component.html',
+  styleUrl: './card-summary.component.scss'
+})
+export class CardSummaryComponent {
+  @Input() card: CardItem = EMPTY_CARD_ITEM;
+}
--- frontend/src/app/card/pages/card-index-page/card-index-page.component.ts
+++ frontend/src/app/card/pages/card-index-page/card-index-page.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { CardListViewComponent } from '@card/components/card-list-view/card-list-view.component';
+import { CardSummaryComponent } from '@card/components/card-summary/card-summary.component';
+import { CardItem, EMPTY_CARD_ITEM } from '@common/types/cardItem';
 
 @Component({
   selector: 'app-card-index-page',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    CardListViewComponent,
+    CardSummaryComponent
+  ],
   templateUrl: './card-index-page.component.html',
-  styleUrl: './card-index-page.component.scss'
+  styleUrl: './card-index-page.component.scss',
 })
-export class CardIndexPageComponent {
+export class CardIndexPageComponent implements OnInit {
+  selectedCard: CardItem = EMPTY_CARD_ITEM;
 
+  ngOnInit() {
+    // Any initial logic if needed
+  }
+
+  onCardSelected(card: CardItem) {
+    this.selectedCard = card;
+  }
 }
