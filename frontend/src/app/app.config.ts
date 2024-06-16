3968e6ea83614c7ef123e2ca3e536973cf35b4b7
Miguel Alanis <anamelessdeath@gmail.com>
2024-06-15 20:15:31 -0600

    new global configurations

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -4,9 +4,17 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { DatePipe } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { LedgerHttpInterceptor } from '@config/interceptors/ledger-http.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),
+  providers: [
+    provideRouter(routes),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LedgerHttpInterceptor,
+      multi: true
+    },
     provideAnimations(),
     DatePipe]
 };
