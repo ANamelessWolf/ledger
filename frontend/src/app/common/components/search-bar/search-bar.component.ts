4dc5289b74729be8d21c48e3a69869a2612b71e7
Miguel Alanis <anamelessdeath@gmail.com>
2024-07-05 20:50:16 -0600

    Add search bar component

---
+++ frontend/src/app/common/components/search-bar/search-bar.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-search-bar',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './search-bar.component.html',
+  styleUrl: './search-bar.component.scss',
+})
+export class SearchBarComponent {
+  searchControl = new FormControl('');
+
+  @Output() search = new EventEmitter<string>();
+
+  onSearch() {
+    console.log(this.searchControl.value);
+    this.search.emit(this.searchControl.value || '');
+  }
+}
